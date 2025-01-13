4d638bb2ba7a05798cae05ab56e3faadfb89f212
Michael Balloni <balloni.michael@yahoo.com>
2025-01-12 21:13:54 -0800

    Implemented load command.  Lots of caching and optimizations.  Short of processing one directory at a time this is pretty lean.

--- src/nldb/main.cpp
+++ src/nldb/main.cpp
@@ -87,13 +87,56 @@ int wmain(int argc, wchar_t* argv[]) {
 			std::wstring dir_path = args[0];
 			printf("Directory: %S\n", dir_path.c_str());
 
-			for (auto const& dir_entry : fs::recursive_directory_iterator(dir_path)) {
-				auto entry_path = dir_entry.path();
+			printf("Setting up...");
+			strings::setup(db);
+			nodes::setup(db);
+			printf("done!\n");
 
-				// FORNOW - Update what we find
-				for (std::wstring path_part : entry_path)
-					printf("/ %S ", path_part.c_str());
-				printf("\n");
+			int64_t file_type_string_id = strings::get_id(db, L"file");
+			int64_t dir_type_string_id = strings::get_id(db, L"dir");
+
+			std::unordered_map<std::wstring, int64_t> string_cache;
+			std::unordered_map<std::string, node> parent_node_cache;
+
+			std::vector<std::wstring> path_parts;
+
+			for (const auto& dir_entry : fs::recursive_directory_iterator(dir_path)) {
+				bool is_file = dir_entry.is_regular_file();
+
+    			path_parts.clear();
+				for (const std::wstring& path_part : dir_entry.path())
+					path_parts.push_back(path_part);
+
+				node cur_node;
+				for (size_t p = 0; p < path_parts.size(); ++p) {
+					const std::wstring& path_part = path_parts[p];
+
+					int64_t path_part_string_id;
+					{
+						const auto str_cache_it = string_cache.find(path_part);
+						if (str_cache_it == string_cache.end()) {
+							path_part_string_id = strings::get_id(db, path_part);
+							string_cache.insert({ path_part, path_part_string_id });
+						}
+						else
+							path_part_string_id = str_cache_it->second;
+					}
+
+					char parent_cache_key[100];
+					sprintf(parent_cache_key, "%I64d_%I64d", cur_node.m_id, path_part_string_id);
+
+					const auto parent_node_it = parent_node_cache.find(parent_cache_key);
+					if (parent_node_it == parent_node_cache.end()) {
+						auto node_in_path = nodes::get_node_in_parent(db, cur_node.m_id, path_part_string_id);
+						if (!node_in_path.has_value())
+							cur_node = nodes::create(db, cur_node.m_id, path_part_string_id, p == path_parts.size() - 1 && is_file ? file_type_string_id : dir_type_string_id);
+						else
+							cur_node = node_in_path.value();
+						parent_node_cache.insert({ parent_cache_key, cur_node });
+					}
+					else
+						cur_node = parent_node_it->second;
+				}
 			}
 
 			return 0;
